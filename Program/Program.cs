df0184787ffa4e6012c2754254a57ecc9fb0f597
MagmaDevil <naver201010@gmail.com>
2025-09-04 21:44:07 +0900

    [2025.9.4]

---
+++ Program/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stat stat = new Stat();
+
+            #region 가비지 컬렉터
+
+            //프로그램이 실행되는 도중에 더 이상 사용되지 않는 메모리를 자동으로
+            //탐색하여 해제하는 기능입니다.
+
+            #endregion
+
+            stat.Wisdom = 5;
+            stat.Strength = 5;
+            stat.Dexterity = 10;
+            stat.Intelligence = 15;
+
+            stat.Describe();
+        }
+    }
+}
---
+++ Program/Stat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Stat
+    {
+        private int wisdom;
+        private int strength;
+        private int dexterity;
+        private int intelligence;
+
+        public int Wisdom { set { wisdom = value; } }
+        public int Strength { set { strength = value; } }
+        public int Dexterity { set { dexterity = value; } }
+        public int Intelligence { set { intelligence = value; } }
+
+        public void Describe()
+        {
+            Console.WriteLine("Wisdom: " + wisdom);
+            Console.WriteLine("Strength: " + strength);
+            Console.WriteLine("Dexterity: " + dexterity);
+            Console.WriteLine("Intelligence: " + intelligence);
+        }
+    }
+}
